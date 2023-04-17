7c191156c1ed66ed142b3917bb6024d05fdb7261
Tooboi <japica123@gmail.com>
2023-04-17 00:17:57 -0400

    Created Nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
   const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
 
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -39,11 +40,31 @@ const Navbar = () => {
         </ul>
         <div className='sm:hidden flex flex-1 justify-end items-center'>
             <img
-            src={menu}
+            src={toggle ? close : menu}
             alt='menu'
             className='w-[28px] h-[28px] object-contain cursor-pointer'
             onClick={() => setToggle(!toggle)}
-            ></img>
+            />
+            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 border-2 border-white bg-primary absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-md`}>
+              <ul className='list-none flex justify-end items-start flex-col gap-4'>
+            {navLinks.map((link) => (
+              <li 
+              key={link.id}
+              className={`${
+                active === link.title
+                ? 'text-white'
+                : 'text-secondary'
+              } font-poppins font-medium cursor-pointer text-[16px]`}
+              onClick={() => {
+                setActive(link.title);
+                setToggle(!toggle);
+                }}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+            </div>
         </div>
       </div>
     </nav>
