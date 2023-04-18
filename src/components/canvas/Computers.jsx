456d1fddc5eae688fef9e4b562c309471de4c3b8
Tooboi <japica123@gmail.com>
2023-04-17 20:04:42 -0400

    added 3D models

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,23 +6,26 @@ import CanvasLoader from '../Loader';
 
 const Computers = () => {
   const computer = useGLTF('./desktop_pc/scene.gltf');
-  
+
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
-      <pointLight intensity={1}/>
-      <primitive object={computer.scene} />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene}
+      scale={0.75}
+      position={[0, -3.25, -1.5]}
+      rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
 
-export default Computers;
 
 
 const ComputersCanvas = () => {
   return (
     <Canvas
-    frameLoop='demand'
+    frameLoop="demand"
     shadows
     camera={{ position: [20, 3, 5], fov: 25 }}
     gl={{ preserveDrawingBuffer: true }}
@@ -39,3 +42,5 @@ const ComputersCanvas = () => {
     </Canvas>
   )
 }
+
+export default ComputersCanvas;
